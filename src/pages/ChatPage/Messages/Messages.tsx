2332220f84ef4b977df7e8a49bf058e633f30256
atryomkorshykau <butovichjunior@gmail.com>
2024-01-03 00:51:55 +0300

    add Chat auto scroll

--- src/pages/ChatPage/Messages/Messages.tsx
+++ src/pages/ChatPage/Messages/Messages.tsx
@@ -2,19 +2,31 @@ import {Message} from "./Message/Message";
 import s from './Messages.module.css'
 import {useSelector} from "react-redux";
 import {getChatMessages} from "../../../utils/selectors/userSelectors";
-import {useEffect, useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 
 export const Messages = () => {
 
     const messages = useSelector(getChatMessages)
 
     const messageAnchorRef = useRef<HTMLDivElement>(null)
+    const [autoScroll, setAutoScroll] = useState(false)
+
+    const scrollHandler = (event: React.UIEvent<HTMLDivElement, UIEvent>) => {
+        let element = event.currentTarget
+        if ((Math.abs(element.scrollHeight - element.scrollTop) - element.clientHeight) < 300) {
+            !autoScroll && setAutoScroll(true)
+        } else {
+            autoScroll && setAutoScroll(false)
+        }
+    }
 
     useEffect(() => {
-        messageAnchorRef.current?.scrollIntoView({behavior: 'smooth'})
+        if (autoScroll) {
+            messageAnchorRef.current?.scrollIntoView({behavior: 'smooth'})
+        }
     }, [messages]);
 
-    return <div className={s.messages}>
+    return <div className={s.messages} onScroll={scrollHandler}>
         {messages.map((m, index) => <Message message={m} key={index}/>)}
         <div ref={messageAnchorRef}></div>
     </div>
