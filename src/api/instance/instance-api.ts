139b76da79a3f2d90c21f1c9189570c6ab16a913
atryomkorshykau <butovichjunior@gmail.com>
2024-01-06 17:44:08 +0300

    users styling

--- src/api/instance/instance-api.ts
+++ src/api/instance/instance-api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const settings = {
-    withCredentials: true,
+    // withCredentials: true,
     headers: {
-        'API-KEY': '88c70fdf-4e66-4d91-860b-59030fc7971d'
+        'API-KEY': '88c70fdf-4e66-4d91-860b-59030fc7971d',
     }
 }
 export const instance = axios.create({baseURL: 'https://social-network.samuraijs.com/api/1.0/', ...settings})
